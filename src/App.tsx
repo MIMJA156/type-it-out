e9964e24713e986152233813b8942cb3bc4d8a95
mimja <75694661+MIMJA156@users.noreply.github.com>
2026-02-05 23:00:23 -0700

    add version display

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { LucideHome, LucideSettings } from "lucide-preact";
-import { useState } from "preact/hooks";
+import { getVersion } from "@tauri-apps/api/app";
+import { useEffect, useState } from "preact/hooks";
 import { create } from "zustand";
 
 import { useLocked } from "./useLocked";
@@ -41,40 +42,48 @@ export const useAppState = create<AppStateStore>((set) => ({
 }));
 
 function App() {
+	const [version, setVersion] = useState("0.0.0");
 	const [ui, setUi] = useState<"home" | "settings" | "help">("home");
 	const locked = useLocked();
 
+	useEffect(() => {
+		getVersion().then(v => setVersion(v));
+	}, []);
+
 	return (
 		<div class={"grid grid-cols-[70px_auto] h-full w-full bg-neutral-900 bg-[url(carbon.png)] bg-size-[55px] bg-blend-darken"}>
-			<div class={"bg-neutral-800 border-r border-neutral-700 flex flex-col items-center gap-6 pt-6"}>
-				<button
-					onClick={() => setUi("home")}
-					disabled={locked}
-					class={"not-disabled:cursor-pointer"}
-				>
-					<LucideHome
-						size={"36px"}
-						class={ui === "home" ? `${locked ? "stroke-zinc-600" : "stroke-zinc-200"}` : `stroke-zinc-400`}
-					/>
-				</button>
+			<div class={"bg-neutral-800 border-r border-neutral-700 flex flex-col justify-between items-center"}>
+				<div class={"flex flex-col gap-6 pt-6"}>
+					<button
+						onClick={() => setUi("home")}
+						disabled={locked}
+						class={"not-disabled:cursor-pointer"}
+					>
+						<LucideHome
+							size={"36px"}
+							class={ui === "home" ? `${locked ? "stroke-zinc-600" : "stroke-zinc-200"}` : `stroke-zinc-400`}
+						/>
+					</button>
 
-				<button
-					onClick={() => setUi("settings")}
-					disabled={locked}
-					class={"not-disabled:cursor-pointer"}
-				>
-					<LucideSettings
-						size={"36px"}
-						class={ui === "settings" ? "stroke-zinc-200" : `${locked ? "stroke-zinc-600" : "stroke-zinc-400"}`}
-					/>
-				</button>
+					<button
+						onClick={() => setUi("settings")}
+						disabled={locked}
+						class={"not-disabled:cursor-pointer"}
+					>
+						<LucideSettings
+							size={"36px"}
+							class={ui === "settings" ? "stroke-zinc-200" : `${locked ? "stroke-zinc-600" : "stroke-zinc-400"}`}
+						/>
+					</button>
 
-				{/* <button onClick={() => setUi("help")} disabled={locked} >
-					<LucideCircleQuestionMark
-						size={"36px"}
-						class={`${ui === "help" ? "stroke-zinc-200" : "stroke-zinc-400"} cursor-pointer`}
-					/>
-				</button> */}
+					{/* <button onClick={() => setUi("help")} disabled={locked} >
+						<LucideCircleQuestionMark
+							size={"36px"}
+							class={`${ui === "help" ? "stroke-zinc-200" : "stroke-zinc-400"} cursor-pointer`}
+						/>
+					</button> */}
+				</div>
+				<span class={"mb-2"}>v{version}</span>
 			</div>
 			<div>
 				{ui === "home" && <Home />}
